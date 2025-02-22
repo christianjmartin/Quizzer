97f3d9cd1384268a2920ef272d3fb803b24b736e
cjmartin232324 <cmart207@uic.edu>
2025-02-22 01:02:14 -0600

    quiz logic finished, 2 immediate TODOs

--- server/server.js
+++ server/server.js
@@ -7,23 +7,23 @@ const PORT = 3000;
 app.use(express.json())
 app.use(express.static(path.join(__dirname, '..', 'app')));
 
-
+// load home page
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'app', 'index.html'));
 });
-app.post('/', (req, res) => {
-    res.send("hello from the backend");
-});
 
+// load quiz page
 app.get('/quiz', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'app', 'quiz.html'));
 });
-app.post('/quiz', async (req, res) => {
+
+// generates a random set of 10 questions from questions.json
+app.get('/getQuiz', async (req, res) => {
     // get random numbers between 0 and 30 for randomly generated questions
     let count = 10;
     let min = 0;
     let max = 29;
-    let random = new Set();
+    let random = new Set(); 
     let questionList = [];
     while (random.size < count) {
         let n = Math.floor(Math.random() * (max - min + 1)) + min;
@@ -34,11 +34,24 @@ app.post('/quiz', async (req, res) => {
         console.log(random);
         const questions = JSON.parse(data);
         random.forEach((randIndex) => questionList.push(questions[randIndex]))
-        // console.log(questionList);
         res.json(questionList);
     } catch (error) {
         console.error("Error reading questions file:", error);
     }
+});
+
+// load results page
+app.get('/results', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'app', 'results.html'));
+});
+
+// process the results of the quiz into the database for the user to see past attempts in menu
+app.post('/results', (req, res) => {
+    const {numCorrect} = req.body;
+    console.log(numCorrect);
+    /*
+    TODO: insert into database quiz results table (SERIAL PK (quiz number), numCorrect, user)
+    */
 });
 
 app.listen(PORT, () => {
