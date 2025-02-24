24692b3de06bc401aa75b091b5315146ca3461f4
cjmartin232324 <cmart207@uic.edu>
2025-02-23 19:59:14 -0600

    start mssql

---
+++ connect.sql
@@ -0,0 +1,12 @@
+-- USE QuizzerDatabase
+-- GO
+
+-- CREATE TABLE Users (
+--     UserID INT PRIMARY KEY IDENTITY(1,1),
+--     Username NVARCHAR(50) NOT NULL,
+--     Email NVARCHAR(100) NOT NULL,
+--     CreatedAt DATETIME DEFAULT GETDATE()
+-- );
+-- GO
+
+SELECT name FROM sys.databases;
